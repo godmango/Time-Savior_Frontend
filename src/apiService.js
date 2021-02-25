18eac3186b03ab63cc25d9c4b8a2d7fa80714c15
Jun Young Cho <godguman@gmail.com>
2021-02-25 12:07:42 +0700

    third test

--- src/apiService.js
+++ src/apiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_API + "api",
+  baseURL: process.env.REACT_APP_BACKEND_API + "/api",
   headers: {
     "Content-Type": "application/json",
   },
